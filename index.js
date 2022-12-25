acc057ca0d70cde294b3ba3f1f7a65ac0d1a427a
raihan512 <raihangazi1024@Gmail.com>
2022-12-25 15:08:11 +0600

    price added to the book details

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 5000
+require('dotenv').config()
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const allbooks = require("./data/books.json");
 const allAuthors = require("./data/authors.json");
