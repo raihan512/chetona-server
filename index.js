774bb0d70a1a7e162f610df631552f77c1f7a88a
raihan512 <raihangazi1024@Gmail.com>
2022-12-30 13:11:49 +0600

    data stored at mongodb and load from there and updated backup data folder with updated data files

--- index.js
+++ index.js
@@ -3,47 +3,68 @@ const app = express()
 var cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+// Connect database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e4yec41.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const allbooks = require("./data/books.json");
-const allAuthors = require("./data/authors.json");
-const categories = require("./data/categories.json");
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+async function run() {
+    const bookCollection = client.db("chetonaprokashon").collection("book");
+    const categoryCollection = client.db("chetonaprokashon").collection("category");
+    const authorCollection = client.db("chetonaprokashon").collection("author");
 
-// Send All Books Data
-app.get('/books', (req, res) => {
-    res.send(allbooks)
-})
-// Send Book Data By Id
-app.get("/book/:id", (req, res) => {
-    const id = req.params.id;
-    const selectedBook = allbooks.find((selectedBook) => selectedBook._id === id);
-    res.send(selectedBook);
-});
-// Send Book Data By cateory name
-app.get("/categories", (req, res) => {
-    res.send(categories);
-});
-app.get("/category/:id", (req, res) => {
-    const id = req.params.id;
-    const selectedCategory = categories.find(category => category._id === id);
-    res.send(selectedCategory);
-})
-// Send All Authors Data
-app.get('/authors', (req, res) => {
-    res.send(allAuthors)
-})
-// Send Author Details By Id
-app.get("/author/:id", (req, res) => {
-    const id = req.params.id;
-    const selectedAuthor = allAuthors.find((author) => author._id === id);
-    res.send(selectedAuthor);
-})
+    try {
+        // Load all books data
+        app.get('/books', async (req, res) => {
+            const query = {}
+            const allbooks = await bookCollection.find(query).toArray();
+            res.send(allbooks)
+        })
+        // Load book data by id
+        app.get("/book/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const selectedBook = await bookCollection.findOne(query);
+            res.send(selectedBook);
+        });
+        // Load all book cateory
+        app.get("/categories", async (req, res) => {
+            const query = {}
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories)
+        });
+        // Load book category by id
+        app.get("/category/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const selectedCategory = await categoryCollection.findOne(query);
+            res.send(selectedCategory);
+        })
+        // Load all authors
+        app.get('/authors', async (req, res) => {
+            const query = {};
+            const allAuthors = await authorCollection.find(query).toArray();
+            res.send(allAuthors)
+        })
+        // Load single author by id
+        app.get("/author/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const selectedAuthor = await authorCollection.findOne(query);
+            res.send(selectedAuthor);
+        })
+    }
+    finally { }
+}
+run().catch()
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
